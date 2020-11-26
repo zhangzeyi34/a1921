da6a2855435bff467d86cc0723e484251fb97263
root <root@LAPTOP-2868O8E3.localdomain>
2020-11-26 22:14:52 +0800

    first commit

---
+++ paixu.h
@@ -0,0 +1,18 @@
+fndef paixu_h
+#define paixu_h
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define N 20
+
+void Bubble1(int a[],int n);
+void Bublle2(int a[],int n);
+void Bubble3(int a[],int n);
+void InsertSort(int a[],int n);
+void SelectSort(int a[],int n);
+int Partition(int a[],int low, int high);
+int Partition2(int a[],int low, int high);
+void QSort(int a[],int low,int high);
+void QuickSort(int a[],int n);
+
+#endif
