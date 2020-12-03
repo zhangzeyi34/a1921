be800b95f7d4711f0b34bd7c604a1569cba05008
root <root@LAPTOP-2868O8E3.localdomain>
2020-12-03 21:19:51 +0800

    fist commit

---
+++ LinkStack.h
@@ -0,0 +1,26 @@
+fndef LinkStack_h
+#define LinkStack_h
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+#include <stdlib.h>
+typedef int dataType;
+typedef struct node{
+	        dataType data;
+		        struct node *next;
+}LinkStack,node;
+
+
+LinkStack* InitStack();
+int Empty(LinkStack *);
+int priority(char x);
+double compute(double x,double y, char op);
+void Push(LinkStack *,dataType x);
+void Pop(LinkStack *);
+dataType GetTop(LinkStack *);
+void Operation();
+
+
+
+#endif
+~
---
+++ testLinkStack.c
@@ -0,0 +1,25 @@
+#include "LinkStack.h"
+int main(){
+	LinkStack *s=InitStack();
+	Push(s,80);
+	Push(s,90);
+	Pop(s);
+	Push(s,70);
+	printf("栈顶元素为：%d\n ",GetTop(s));
+	Operation();
+	printf("\n");
+	return 0;
+}
+#include "LinkStack.h"
+int main(){
+	LinkStack *s=InitStack();
+	Push(s,80);
+	Push(s,90);
+	Pop(s);
+	Push(s,70);
+	printf("栈顶元素为：%d\n ",GetTop(s));
+	Operation();
+	printf("\n");
+	return 0;
+}
+
